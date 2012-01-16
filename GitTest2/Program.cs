47fafd43eb5812cd7edb13575a99dd1f23a71c80
Tom Whitner <whitner.dev@gmail.com>
2012-01-16 17:06:16 -0500

    initial commit

---
+++ GitTest2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git test 2...");
+        }
+    }
+}
