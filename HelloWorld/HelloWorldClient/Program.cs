dc2a6288a7ee9d631a25e1ae3013d18152030176
Tom Whitner <whitner.dev@gmail.com>
2012-01-24 21:21:31 -0500

    client implemented

---
+++ HelloWorld/HelloWorldClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HelloWorldServiceClient();
+            Console.WriteLine(client.GetMessage("Tom Whitner"));
+            Console.ReadLine();
+        }
+    }
+}
