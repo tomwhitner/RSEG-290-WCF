12f0e03967167293785f0227d5d193174ff97da2
Tom Whitner <whitner.dev@gmail.com>
2012-01-31 14:09:58 -0500

    added command line service host

---
+++ HelloWorld/HostCmdLineApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using System.Configuration;
+
+namespace HostCmdLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type serviceType = typeof(MyWCFServices.HelloWorldService);
+            string httpBaseAddress = ConfigurationManager.AppSettings["HTTPBaseAddress"];
+            var baseAddress = new Uri[] { new Uri(httpBaseAddress) };
+            var host = new ServiceHost(serviceType, baseAddress);
+            host.Open();
+            Console.WriteLine("HelloWorldService is now running. ");
+            Console.WriteLine("Press any key to stop it ...");
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
