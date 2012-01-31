e30f8a546461de3d40bb87cc115ec9f42caca1cc
Tom Whitner <whitner.dev@gmail.com>
2012-01-31 18:14:44 -0500

    finished majority of hw1 - zipcode service.  need quick review, though.

--- Homework1/ZipCodeService/ZipCodeService.cs
+++ Homework1/ZipCodeService/ZipCodeService.cs
@@ -1,29 +1,28 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Hosting;
 
 namespace ZipCodeService
 {
     public class ZipCodeService : IZipCodeService
     {
-        private const string UnauthorizedUser = "Unauthorizeduser";
+        private const string UnauthorizedUser = "Unauthorized user";
         private const string UnknownZipCode = "Unknown zip code";
 
         public string Lookup(string caller, string zipcode)
         {
-            string result = null;
+            string result;
             if (IsAuthorized(caller))
             {
                 // do lookup
-
-                result = UnknownZipCode;
-
-
+                result = ZipCodes.ContainsKey(zipcode) ? ZipCodes[zipcode] : UnknownZipCode;
             }
             else
             {
                 result = UnauthorizedUser;
             }
 
-            LogResults(result);
+            LogResults(caller, zipcode, result);
 
             return result;
         }
@@ -31,12 +30,59 @@ namespace ZipCodeService
 
         private bool IsAuthorized(string caller)
         {
-            return true;
+            return Users.Contains(caller);
         }
 
-        private void LogResults(string results)
+        private void LogResults(string caller, string zipcode, string results)
         {
-            // log it somehow
+            var tw = new StreamWriter(HostingEnvironment.MapPath("~/log.txt"), true);
+            tw.WriteLine(string.Format("Caller: {0}, ZipCode: {1}, Results: {2}", caller, zipcode, results));
+            tw.Close();
+        }
+
+        private List<string> _users;
+
+        private List<string> Users
+        {
+            get
+            {
+                if (_users == null)
+                {
+                    _users = new List<string>();
+                    var tr = new StreamReader(HostingEnvironment.MapPath("~/users.txt"));
+                    while (!tr.EndOfStream)
+                    {
+                        _users.Add(tr.ReadLine());
+                    }
+                    tr.Close();
+                }
+                return _users;
+            }
+        }
+
+        private Dictionary<string, string> _zipcodes;
+
+        private Dictionary<string, string> ZipCodes
+        {
+            get
+            {
+                if (_zipcodes == null)
+                {
+                    _zipcodes = new Dictionary<string, string>();
+                    var tr = new StreamReader(HostingEnvironment.MapPath("~/zipcodes.txt"));
+                    while (!tr.EndOfStream)
+                    {
+                        var line = tr.ReadLine();
+                        if (line != null)
+                        {
+                            var parts = line.Split('|');
+                            _zipcodes.Add(parts[0], parts[1]);
+                        }
+                    }
+                    tr.Close();
+                }
+                return _zipcodes;
+            }
         }
     }
 }
